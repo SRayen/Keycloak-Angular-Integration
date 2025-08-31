6b2000cafbdde256b72ede3841e6aea8c109e3b9
S Ray <selmen.rayen.92@gmail.com>
2025-08-31 12:38:26 +0100

    feat: updated app.config.ts to include keycloak and http client configuration

--- frontend/dny-netflix/src/app/app.config.ts
+++ frontend/dny-netflix/src/app/app.config.ts
@@ -1,8 +1,36 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { KeycloakService, KeycloakBearerInterceptor } from 'keycloak-angular';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { initializeKeycloak } from './init/keycloak.init.factory';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    KeycloakService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: KeycloakBearerInterceptor,
+      multi: true,
+    },
+    provideHttpClient(
+      withInterceptorsFromDi() // tell httpClient to use interceptors from DI
+    ),
+  ],
 };
